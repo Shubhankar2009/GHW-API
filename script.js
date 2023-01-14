85387ce1fd2b94deaee396f8db9f25d2b0e670d8
Shubhankar <97114934+Shubhankar2009@users.noreply.github.com>
2023-01-15 01:54:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function Click() {
+    fetch('https://icanhazdadjoke.com/slack')
+        .then(data => data.json())
+        .then(jokeData => {
+            const jokeText = jokeData.attachments[0].text;
+            const jokeElement = document.getElementById('joke');
+
+
+            jokeElement.innerHTML = `${jokeText} 😂 😂`;
+        })
+}
